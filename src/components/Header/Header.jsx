4487e4e05268585c04e61ff4215ffe82833057e2
shubham21699 <shubham21699@gmail.com>
2022-07-04 01:07:30 +0530

    Header page made.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,31 @@
 import React from "react";
+import CTA from "./CTA";
+import Me from "../../assets/me.png";
 import "./Header.css";
+import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h4>Hello! I'm</h4>
+        <h1>Shubham Goel</h1>
+        <h5 className="text-light">Frontend Developer</h5>
+        <h5 className="text-light">
+          Final Year Computer Engineering Undergrad at Thapar Institute of
+          Engineering and Technology
+        </h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={Me} alt="Me" />
+        </div>
+        <a href="#contact" className="scroll__down">
+          Scroll To Bottom
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
