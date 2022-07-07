08221c7dcd3d95d3a56ebebdcd2952888631e6cd
shubham21699 <shubham21699@gmail.com>
2022-07-07 17:17:58 +0530

    Email sent functionality added.

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Contact.css";
 import { MdOutlineEmail } from "react-icons/md";
 import { BsWhatsapp } from "react-icons/bs";
+import emailjs from "emailjs-com";
+import MuiAlert from "@mui/material/Alert";
+import Snackbar from "@mui/material/Snackbar";
 
 const Contact = () => {
+  const Alert = React.forwardRef(function Alert(props, ref) {
+    return (
+      <MuiAlert
+        elevation={6}
+        ref={ref}
+        variant="filled"
+        vertical="top"
+        horizontal="center"
+        {...props}
+      />
+    );
+  });
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      "service_i6ytjhi",
+      "template_o6zh8ul",
+      form.current,
+      "eZ7HFs3Tm9PCaamVi"
+    );
+
+    handleClick();
+    e.target.reset();
+  };
+
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
@@ -12,23 +57,32 @@ const Contact = () => {
         <div className="contact__options">
           <article className="contact__option">
             <MdOutlineEmail size={30} />
-            <h4>Email</h4>
-            <h5>Personal ID : shubham21699@gmail.com</h5>
-            <h5>College ID : sgoel3_be19@thapar.edu</h5>
-            <a href="mailto:shubham21699@gmail.com">
-              <i>Send me Email</i>
-            </a>
+            <h4>Send Email To</h4>
+            <h5>
+              Personal ID :
+              <a href="mailto:shubham21699@gmail.com" className="emailTag">
+                <i>shubham21699@gmail.com</i>
+              </a>
+            </h5>
+            <h5>
+              College ID :
+              <a href="mailto:sgoel3_be19@thapar.edu" className="emailTag">
+                <i>sgoel3_be19@thapar.edu</i>
+              </a>
+            </h5>
           </article>
           <article className="contact__option">
             <BsWhatsapp size={30} />
             <h4>Mobile Number</h4>
-            <h5>{"(+91) 9855900606"}</h5>
+            <h5>
+              <i>{"(+91) 9855900606"}</i>{" "}
+            </h5>
             <a href="https://wa.me/+919855900606" target="_blank">
               <i>Send Message on WhatsApp</i>
             </a>
           </article>
         </div>
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input
             type="text"
             name="name"
@@ -45,6 +99,19 @@ const Contact = () => {
           <button type="submit" className="btn btn-primary">
             Send Message
           </button>
+          <Snackbar
+            open={open}
+            autoHideDuration={2000}
+            anchorOrigin={{
+              horizontal: "right",
+              vertical: "top",
+            }}
+            onClose={handleClose}
+          >
+            <Alert severity="success" sx={{ width: "100%" }}>
+              Message sent successfully!
+            </Alert>
+          </Snackbar>
         </form>
       </div>
     </section>
